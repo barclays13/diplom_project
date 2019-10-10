2242404e36ddb9a5b848f2bb0c392f11ba0f6c67
Zubarev Sergey <barclays13@mail.ru>
2019-10-11 01:09:20 +0300

    phone

--- src/modules/validationForms.js
+++ src/modules/validationForms.js
@@ -2,8 +2,13 @@ const validationForms = () => {
 
     document.getElementsByName('name').forEach((element) => {
         element.addEventListener('input', () => {
-            console.log(0);
             element.value = element.value.replace(/[^а-яё]/i, '');
+        });
+    });
+    document.getElementsByName('phone').forEach((element) => {
+        element.addEventListener('input', () => {
+            element.value = element.value.replace(/[^0-9+]/, '');
+
         });
     });
     
@@ -11,13 +16,3 @@ const validationForms = () => {
 };
 export default validationForms;
 
-//\+375[0-9]{9}$
-    /*
-    document.getElementsByName('phone').forEach((element) => {
-        element.addEventListener('input', () => {
-            console.log(2);
-            element.value = element.value.replace(/[^375]{3}/, '');
-            //element.value = element.value.replace(/+375[0-9+]/, '');
-        });
-    });
-    */
